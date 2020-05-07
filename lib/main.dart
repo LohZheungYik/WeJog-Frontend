b88dc446c159099d0d16ffc4247bd0e82a3d90f1
LohZheungYik <62550487+LohZheungYik@users.noreply.github.com>
2020-05-07 19:00:35 +0800

    Add files via upload
    change start screen back to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wejog1/screens/home/index.dart';
-import 'package:wejog1/screens/home/tabs/bloodPressureTracker.dart';
-import 'package:wejog1/screens/home/tabs/calorieTracker.dart';
-import 'package:wejog1/screens/home/tabs/searchFood.dart';
-import 'package:wejog1/screens/home/tabs/settings.dart';
-import 'package:wejog1/screens/home/tabs/todo.dart';
-import 'package:wejog1/screens/login/login.dart';
-import 'package:wejog1/screens/login/register.dart';
 import 'package:wejog1/screens/splashScreen/Splash.dart';
 
 /*main function*/
@@ -17,11 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     //home: BloodPressureTracker(),
-     home: Login(),
-     //home: Index(), //launch splash screen
-      //home: Register(),
-      //home: Settings(),
+      home: SplashScr(), //launch splash screen
     );
   }
 }
